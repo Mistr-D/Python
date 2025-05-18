97f74c5687b5fd057b55af2475cd8d1cbb27bca5
Mistr-D <davidhruska092cz@gmail.com>
2025-05-18 10:10:03 +0200

    Update obsah_trojuhelniku.py

--- obsah_trojuhelniku.py
+++ obsah_trojuhelniku.py
@@ -2,7 +2,6 @@ def obsah_trojuhelniku(a, v):
 
     return (a * v) / 2
 
-# Příklad použití
 if __name__ == "__main__":
     a = float(input("Zadejte délku základny trojúhelníku (a): "))
     v = float(input("Zadejte výšku trojúhelníku (v): "))
