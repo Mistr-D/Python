f9af28abad97f4e476fba169fa58f29ea22df47d
Mistr-D <davidhruska092cz@gmail.com>
2025-05-18 09:57:28 +0200

    Delete obsah_trojuhelniku.py

--- obsah_trojuhelniku.py
+++
@@ -1,9 +0,0 @@
-def obsah_trojuhelniku(a, v):
-
-    return (a * v) / 2
-
-# Příklad použití
-if __name__ == "__main__":
-    a = float(input("Zadejte délku základny trojúhelníku (a): "))
-    v = float(input("Zadejte výšku trojúhelníku (v): "))
-    print(f"Obsah trojúhelníku je: {obsah_trojuhelniku(a, v)}")
