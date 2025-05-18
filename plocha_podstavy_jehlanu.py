8230420786ff57b7de1e3fc5d86dd07a3ba56009
Mistr-D <davidhruska092cz@gmail.com>
2025-05-18 09:57:57 +0200

    Delete plocha_podstavy_jehlanu.py

--- plocha_podstavy_jehlanu.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def plocha_podstavy_jehlanu(a, b):
-    return a * b
-if __name__ == "__main__":
-    a = float(input("Zadejte délku první strany podstavy (a): "))
-    b = float(input("Zadejte délku druhé strany podstavy (b): "))
-    plocha = plocha_podstavy_jehlanu(a, b)
-    print(f"Plocha podstavy jehlanu je: {plocha}")
